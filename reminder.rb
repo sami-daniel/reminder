661acd170e4fe6239611894e8a6bb8c9cea1e691
Sami Daniel <samidanielpersonal@gmail.com>
2025-02-09 15:36:39 -0300

    Define project scope with pseudo-code

--- reminder.rb
+++ reminder.rb
@@ -4,3 +4,8 @@ cmd = ARGV[0]
 
 raise ArgumentError.new 'Invalid argument for reminder' if cmd.nil?
 
+# if argument == save: ModuleName::save(...)
+# if argument == delete: ModuleName::delete(...)
+# if argument == update: ModuleName::update(...)
+# if argument == list: ModuleName::list(...)
+# if argument == list-filtered: ModuleName::list-filtered(...)
